161f7dc7eef54527e809cf7d99beb03264788003
MarchelloLemonchello <Marchello.lemonchello@yandex.ru>
2025-04-20 02:03:55 +0300

    лого

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,7 @@
 export const Auth = () => {
     return (
         <div style={{width:'100%', height:'100%', backgroundColor: '#8cb159', boxSizing: 'border-box', paddingTop:'20vh' }}>
-            <img style={{ width:'280px', height:'150px', display: 'block', margin: '0 auto'}} src="/LogoNFT.png" alt=""/>
+                <img style={{ width:'550', height:'200px', display: 'block', margin: '0 auto'}} src="/LogoNFT.png" alt=""/>
         </div>
     );
 };
